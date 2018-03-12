5b5c7f2655806f49ad797f83112beef170b6be0c
abricker <abricker@wustl.edu>
2018-03-11 21:41:12 -0500

    Backing up before messing with GUI

--- src/KNDL-Randomize.py
+++ src/KNDL-Randomize.py
@@ -9,16 +9,16 @@ from mainwindow import Ui_MainWindow
 VERSION = '1.1.0'
 
 # Valid byte values for Kirby's ability
-ability_values = ["00","01","02","03","04","05","06","07","08","09","0A","0B","0C",
+abilityValues = ["00","01","02","03","04","05","06","07","08","09","0A","0B","0C",
                 "0D","0E","0F","10","11","12","13","14","15","16","17","18"]
 
 # ROM locations of enemy abilities
-ability_locations = ["7417C4", "7429F4", "740C38", "743344", "742A20", "7417F0", "742A4C",
+abilityLocations = ["7417C4", "7429F4", "740C38", "743344", "742A20", "7417F0", "742A4C",
                      "7418A0", "740C64", "740C90", "7418F8", "742B28", "741924", "741950",
                      "740CE8", "740D14", "741A58", "741A84", "740D6C", "740D98", "740DC4"]
 
 # ROM locations of Kirby's palette
-palette_locations = ["DC62A", "DC8AA", "DC96C", "DCB2A", "DCBAA", "DD0C4", "DD0E6", "DD23A", "DD306",
+kirbyPaletteLocations = ["DC62A", "DC8AA", "DC96C", "DCB2A", "DCBAA", "DD0C4", "DD0E6", "DD23A", "DD306",
                      "E7418", "E745C", "E74A0", "E9D5C", "E9D7E", "E9ED2", "E9F9E", "F997C", "F997C",
                      "F99C0", "F9A04", "FEFFC", "FF01E", "FF172", "FF23E", "108364", "1083A8", "1083EC",
                      "10C260", "10C282", "10C3D6", "10C4A2", "123604", "123648", "12368C", "12A6DC",
@@ -47,38 +47,43 @@ metaKnightLocations = ["226278", "227A60", "2290C0", "22B2AC", "22EB7C", "23118C
                        "27D826", "2DFFEC", "2E000E", "2E24BC", "2E24DE", "2E692C", "2EA4BC", "2EA758",
                        "2EE490", "2F0660", "2F1A5C", "2F3242"]
 
+metaKnightSwordLocations = ["226298", "227A80", "2290E0", "22B2CC", "22EB9C", "2311AC", "231BFC",
+                            "232050", "2352D4", "235BD0", "235BF2", "235C34", "235C76", "238BE6",
+                            "23ADA8", "23E964", "27D846", "2E002E", "2E24FE", "2E694C", "2EA4DC",
+                            "2EA778", "2EE4B0", "2F0680", "2F1A7C", "2F3262"]
+
 # Palettes need to be in the same order as in the GUI
-new_palette = ["2104BF563F203C18571053084E082A043F543C383718", # Red
-               "2104DF07FF129F121F169F193911D10C5F05F9049104", # Orange
-               "2104FF077F07FF06DD061606B1054C059F047C043604", # Yellow
-               "2104F953F01BCF136D07880606068305FC055A051205", # Green
-               "21047F7F7F721F66BF55FC3C74206D0CA432272EC621", # Cherry
-               "2104F873EF63684FA13A2136C12D6121BF2A1C169805", # Emerald
-               "2104F57F927F0E7F8B7E067EA365E6408D7D2A71875C", # Ocean
-               "2104577F907A2D72EB69615D0151A1402B60294C2838", # Sapphire
-               "21041A7F977EF57D727D4F6CCD05AA383745B338302C", # Grape
-               "21049B4A3B36D82D7425111DCE148B10B41071082F04", # Chocolate
-               "2104FF7F9C7318639452EF3D6B2DE71C10428C312925", # Chalk
-               "2104FF7FFF7F9C731863734EEF3D6B2D5F35BB243718", # Snow
-               "21043146EF3DAD356B2D29250821C6189F0A1F067A05", # Carbon
-               "0000324A1146CE398C314A290821C6184A29E71CA514", # Mirror
-               "00005D635D63784E784EB235EC18E71C7D2D171DF11C", # Stone
-               "0000FF7FFA7FF57F347B6C7A8779E578357911791179", # Ice
-               "0000BD77607460744354435427282728DA48B5449138", # GBA Meta Knight
-               "5325FB417D733C679C5AFB41F841D3397A29F914B410", # KDL3 Pink
-               "494DF06D7C7739739672F06DF061CE4D6A64E564A450", # KDL3 Blue
-               "00003D3FBD14BD14D914D914B20CAD593D3F5B32B925", # Waddle Dee
-               "0000DE7B2A7664756475E16C405C2038D66AEF51083D", # Lololo
-               "0000DE7BDE451C2D1C2DB6186F0C2700F67A306E2861", # Lalala
-               "0000DE7BFF7F7B6FF75E524ACE394A297B6FF75E524A"] # Grayscale
+kirbyPalettes = ["2104BF563F203C18571053084E082A043F543C383718", # Red
+                 "2104DF07FF129F121F169F193911D10C5F05F9049104", # Orange
+                 "2104FF077F07FF06DD061606B1054C059F047C043604", # Yellow
+                 "2104F953F01BCF136D07880606068305FC055A051205", # Green
+                 "21047F7F7F721F66BF55FC3C74206D0CA432272EC621", # Cherry
+                 "2104F873EF63684FA13A2136C12D6121BF2A1C169805", # Emerald
+                 "2104F57F927F0E7F8B7E067EA365E6408D7D2A71875C", # Ocean
+                 "2104577F907A2D72EB69615D0151A1402B60294C2838", # Sapphire
+                 "21041A7F977EF57D727D4F6CCD05AA383745B338302C", # Grape
+                 "21049B4A3B36D82D7425111DCE148B10B41071082F04", # Chocolate
+                 "2104FF7F9C7318639452EF3D6B2DE71C10428C312925", # Chalk
+                 "2104FF7FFF7F9C731863734EEF3D6B2D5F35BB243718", # Snow
+                 "21043146EF3DAD356B2D29250821C6189F0A1F067A05", # Carbon
+                 "0000324A1146CE398C314A290821C6184A29E71CA514", # Mirror
+                 "00005D635D63784E784EB235EC18E71C7D2D171DF11C", # Stone
+                 "0000FF7FFA7FF57F347B6C7A8779E578357911791179", # Ice
+                 "0000BD77607460744354435427282728DA48B5449138", # GBA Meta Knight
+                 "5325FB417D733C679C5AFB41F841D3397A29F914B410", # KDL3 Pink
+                 "494DF06D7C7739739672F06DF061CE4D6A64E564A450", # KDL3 Blue
+                 "00003D3FBD14BD14D914D914B20CAD593D3F5B32B925", # Waddle Dee
+                 "0000DE7B2A7664756475E16C405C2038D66AEF51083D", # Lololo
+                 "0000DE7BDE451C2D1C2DB6186F0C2700F67A306E2861", # Lalala
+                 "0000DE7BFF7F7B6FF75E524ACE394A297B6FF75E524A"] # Grayscale
 
-metaKnightPalette = ["0000397F317E8E6D0C59DE513B39082C1D4C1848133CFF03DF025702FF7F", # Pink
-                     "0000397F317E8E6D0C59FF7F7B6F082C5B6FD65A324AFF03DF025702FF7F", # White
-                     "0000397F317E8E6D0C59DD20B918082CB2108F084D04FF03DF025702FF7F", # Red
-                     "0000397F317E8E6D0C596932092A082CB2108F084D04FF03DF025702FF7F", # Green
-                     "21048C318C314B2D8410630C210421042104210421048C316B2DA514EF3D"] # Mirror
+metaKnightPalettes = ["0000397F317E8E6D0C59DE513B39082C1D4C1848133CFF03DF025702FF7F", # Pink
+                      "0000397F317E8E6D0C59FF7F7B6F082C5B6FD65A324AFF03DF025702FF7F", # White
+                      "0000397F317E8E6D0C59DD20B918082CB2108F084D04FF03DF025702FF7F", # Red
+                      "0000397F317E8E6D0C596932092A082CB2108F084D04FF03DF025702FF7F", # Green
+                      "21048C318C314B2D8410630C210421042104210421048C316B2DA514EF3D"] # Mirror
 
-# TODO: Add color change for Meta Knight sword
+metaKnightSwordPalettes = ["0000D25F2D4F69360732"] # Green
 
 # Creating a custom exception, how fancy
 class HashError(Exception):
@@ -88,7 +93,7 @@ class KirbyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(KirbyApp, self).__init__(parent)
         self.setupUi(self)
-        self.findROMButton.clicked.connect(self.open_file)
+        self.findROMButton.clicked.connect(self.openFile)
         self.randomizeButton.clicked.connect(self.runRandomizer)
         self.title.setText(self.title.text() + VERSION)
 
@@ -111,32 +116,32 @@ class KirbyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             lambda checked: checked and self.enemyCheck.setChecked(False))
 
     # Opens ROM selector window, clears the previous path text
-    def open_file(self):
+    def openFile(self):
         self.romDisplay.clear()
-        self.rom_file = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", os.path.dirname(__file__), "GBA ROMs (*.gba)")[0]
-        if self.rom_file:
-            self.romDisplay.setText(self.rom_file)
+        self.romFile = QtWidgets.QFileDialog.getOpenFileName(self, "Open file", os.path.dirname(__file__), "GBA ROMs (*.gba)")[0]
+        if self.romFile:
+            self.romDisplay.setText(self.romFile)
 
     def getKirbyColor(self):
         c = self.kirbyComboBox.currentIndex()
-        if c == (len(new_palette) + 1):
-            return random.randint(0, len(new_palette)-1)
+        if c == (len(kirbyPalettes) + 1):
+            return random.randint(0, len(kirbyPalettes)-1)
         return (c - 1)
 
     def getMKColor(self):
         c = self.MKcomboBox.currentIndex()
-        if c == (len(metaKnightPalette) + 1):
-            return random.randint(0, len(metaKnightPalette)-1)
+        if c == (len(metaKnightPalettes) + 1):
+            return random.randint(0, len(metaKnightPalettes)-1)
         return (c - 1)
 
     def runRandomizer(self):
-        try:
-            rom = open(self.rom_file, 'rb').read()
-            test_hash = hashlib.md5(rom).hexdigest()
+        ry:
+            rom = open(self.romFile, 'rb').read()
+            testHash = hashlib.md5(rom).hexdigest()
             # Checks for the correct ROM
-            if test_hash != "35ae64b0f27e60107c14ab956f6cdf70":
+            if testHash != "35ae64b0f27e60107c14ab956f6cdf70":
                 raise HashError("Invalid checksum")
-            rom_list = list(rom)
+            romList = list(rom)
 
             # Uses given input as seed, else randomly picks a new seed to use
             KNDL_seed = self.seedValue.text()
@@ -146,55 +151,55 @@ class KirbyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
             if self.enemyCheck.isChecked():
                 if self.starRodCheck.isChecked():
-                    ability_values.append("19")
+                    abilityValues.append("19")
 
                 # Gives enemies new abilities based on random selection from file
-                for item in ability_locations:
+                for item in abilityLocations:
                     address = int(item, 16)
-                    new_enemy = random.choice(ability_values)
+                    new_enemy = random.choice(abilityValues)
                     new_enemy = int(new_enemy,16)
-                    rom_list[address] = new_enemy
+                    romList[address] = new_enemy
 
             elif self.noAbilitiesCheck.isChecked():
-                for item in ability_locations:
+                for item in abilityLocations:
                     address = int(item, 16)
-                    rom_list[address] = 0 # Every enemy should be given the "0x00" value, or no ability
+                    romList[address] = 0 # Every enemy should be given the "0x00" value, or no ability
 
             if self.kirbyComboBox.currentText() != "Default (Pink)":
                 new_color = self.getKirbyColor()
-                row = new_palette[new_color]
+                row = kirbyPalettes[new_color]
                 new_colors = []
                 for i in range(0, len(row), 2):
                     new_colors.append(int(row[i:i+2],16))
 
                 # Replaces old color palettes with the new
-                for item in palette_locations:
+                for item in kirbyPaletteLocations:
                     color_address = int(item, 16)
                     for i in range(0, len(new_colors)):
-                        rom_list[color_address + i] = new_colors[i]
+                        romList[color_address + i] = new_colors[i]
 
                 # Want to change the color of the life icon to match
                 # TODO: Someday make this nicer
-                rom_list[int("5A56F2", 16)] = new_colors[0] # 16th byte - Outline
-                rom_list[int("5A56F3", 16)] = new_colors[1]
-                rom_list[int("5A56EE", 16)] = new_colors[4] # 14th byte - Main body
-                rom_list[int("5A56EF", 16)] = new_colors[5]
-                rom_list[int("5A56EA", 16)] = new_colors[12] # 12th byte - Body highlight
-                rom_list[int("5A56EB", 16)] = new_colors[13]
-                rom_list[int("5A56D8", 16)] = new_colors[18] # 3rd byte - Feet
-                rom_list[int("5A56D9", 16)] = new_colors[19]
+                romList[int("5A56F2", 16)] = new_colors[0] # 16th byte - Outline
+                romList[int("5A56F3", 16)] = new_colors[1]
+                romList[int("5A56EE", 16)] = new_colors[4] # 14th byte - Main body
+                romList[int("5A56EF", 16)] = new_colors[5]
+                romList[int("5A56EA", 16)] = new_colors[12] # 12th byte - Body highlight
+                romList[int("5A56EB", 16)] = new_colors[13]
+                romList[int("5A56D8", 16)] = new_colors[18] # 3rd byte - Feet
+                romList[int("5A56D9", 16)] = new_colors[19]
 
                 # Changing the life icon also messes with the health bar palette, so that needs to be changed too
-                rom_list[int("5A56D6", 16)] = new_colors[20]
-                rom_list[int("5A56D7", 16)] = new_colors[21]
-                rom_list[int("5A56DA", 16)] = new_colors[16]
-                rom_list[int("5A56DB", 16)] = new_colors[17]
-                rom_list[int("5A56DC", 16)] = new_colors[16]
-                rom_list[int("5A56DD", 17)] = new_colors[17]
+                romList[int("5A56D6", 16)] = new_colors[20]
+                romList[int("5A56D7", 16)] = new_colors[21]
+                romList[int("5A56DA", 16)] = new_colors[16]
+                romList[int("5A56DB", 16)] = new_colors[17]
+                romList[int("5A56DC", 16)] = new_colors[16]
+                romList[int("5A56DD", 17)] = new_colors[17]
 
             if self.MKcomboBox.currentText() != "Default":
                 new_color = self.getMKColor()
-                row = metaKnightPalette[new_color]
+                row = metaKnightPalettes[new_color]
                 new_colors = []
                 for i in range(0, len(row), 2):
                     new_colors.append(int(row[i:i+2],16))
@@ -203,10 +208,10 @@ class KirbyApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 for item in metaKnightLocations:
                     color_address = int(item, 16)
                     for i in range(0, len(new_colors)):
-                        rom_list[color_address + i] = new_colors[i]
+                        romList[color_address + i] = new_colors[i]
 
-            rom = bytes(rom_list)
-            new_rom = open('.'.join(self.rom_file.split(".")[:-1]) + "_" + str(KNDL_seed) + ".gba", 'wb')
+            rom = bytes(romList)
+            new_rom = open('.'.join(self.romFile.split(".")[:-1]) + "_" + str(KNDL_seed) + ".gba", 'wb')
             new_rom.write(rom)
             new_rom.close()
 
